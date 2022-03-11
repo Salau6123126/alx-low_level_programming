26c72282efd1d5d7be664b2e521629e372b3947c
Salau Fawaz <salau1512113945@gmail.com>
2022-03-11 16:57:41 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for(; num < 10; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
