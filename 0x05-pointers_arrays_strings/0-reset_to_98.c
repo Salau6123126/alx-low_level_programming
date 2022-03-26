2ecfa2d372050c61188818e13e391df295c25374
Salau Fawaz <salau1512113945@gmail.com>
2022-03-26 20:05:51 +0100

    i

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,12 @@
 
 /**
  * reset_to_98 - tikes a poi
- *
- * @n - pointer character
+ * Description: Updates value to 98
+ * @n - pointer to n
  *
  * Return: Always (0)
  */
 void reset_to_98(int *n);
 {
 	*n = 98;
-
-	return (0);
 }
