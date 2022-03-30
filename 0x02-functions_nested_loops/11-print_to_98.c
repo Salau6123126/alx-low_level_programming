51555879522112313b1d165375ee061b0b72ad1d
Salau Fawaz <salau1512113945@gmail.com>
2022-03-30 21:21:31 +0100

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n - the first term
+ * Return: Always (0)
+ */
+void print_to_98(int n)
+{
+	int n;
+	for (i = n; i < 99; i++)
+	{
+		printf("%d", n);
+		putchar(',');
+	}
+	purchar('\n');
+
+	return (98);
+}
