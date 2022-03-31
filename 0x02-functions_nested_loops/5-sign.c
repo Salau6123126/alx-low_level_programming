d17c16353f9a65d830c237aefd1097d31915625d
Salau Fawaz <salau1512113945@gmail.com>
2022-03-31 15:49:16 +0100

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_sign -- Prints the sign of a number
  * @n: number to be processed
@@ -10,7 +10,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		
+
 		return (1);
 	}
 	else if (n == 0)
