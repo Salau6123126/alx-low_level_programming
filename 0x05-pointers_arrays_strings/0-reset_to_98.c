5eb8d01055b3e000a1d2fc346b5be3ac2c2fc941
Fawaz Salau <66675697+Salau6123126@users.noreply.github.com>
2022-04-02 13:12:52 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - tikes a poi
- *Description: Updates value to 98
- * @n - pointer n
- * 
+ * Description: Updates value to 98
+ * @n: pointer n
+ *
  */
 void reset_to_98(int *n)
 {
