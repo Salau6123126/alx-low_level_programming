d0cef8a404662f6286bde861ada95ae685ea514c
Fawaz Salau <66675697+Salau6123126@users.noreply.github.com>
2022-04-02 13:24:12 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints a string
@@ -8,8 +9,8 @@ void _puts(char *str)
 {
 	while (*str)
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
