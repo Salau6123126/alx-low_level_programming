1fc51209939bec227237e5359a74acea831526b6
Fawaz Salau <66675697+Salau6123126@users.noreply.github.com>
2022-04-02 12:34:44 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,26 @@
 #include "main.h"
+/**
+* 8-24_hours - prints a 24 hour timer
+* Description: prints every minute
+*/
+void 8-24_hours(void)
+{
+	int time;
+	int x;
+
+	time = 0;
+
+	while (time < 24)
+	{
+		for (x = 0; x < 60; x++)
+		{
+			_putchar(time / 10 + 48);
+			_putchar(time % 10 + 48);
+			_putchar(':');
+			_putchar(x / 10 + 48);
+			_putchar(x % 10 + 48);
+			_putchar('\n');
+		}
+		time++;
+	}
+}
