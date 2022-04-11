8d6026d317f8c95a71f2c00624a1550d5db4a3a9
Salau Fawaz <salau1512113945@gmail.com>
2022-04-11 17:08:48 +0100

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file compiled from
+ *
+ * Return: (0)
+ */
+int main()
+{
+	printf("The current file name is %s\n",  __FILE__);
+	
+	return (0);
+}
