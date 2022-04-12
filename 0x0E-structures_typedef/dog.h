78ad037f7eabed3ffc315f6fa07d74de8eb27966
Salau Fawaz <salau1512113945@gmail.com>
2022-04-12 15:26:50 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef GUARD
+#define GUARD
+#include <stdio.h>
+
+/**
+ * struct dog - My first struct definition
+ * @name: Pointer to name
+ * @age: Age
+ * @owner: Pointer to owner
+ *
+ * Description: Nothing really
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
