436cafd078dd4a5fe931528e74216ad7e1977713
Salau Fawaz <salau1512113945@gmail.com>
2022-04-12 15:53:25 +0100

    Update, most have forgotten

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef GUARD
 #define GUARD
-#include <stdio.h>
 
 /**
  * struct dog - My first struct definition
@@ -15,5 +14,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*typedef struct dog dog_t;*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
